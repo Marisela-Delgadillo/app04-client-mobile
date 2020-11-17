3bd2c19b2cb7838270864068cf84f4da3b757b84
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-11-17 13:35:41 -0700

    mobile

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import {StyleSheet, View } from 'react-native';
-import Routes from './components/Routes';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default class App extends Component {
+export default class App extends Component 
+{
+  state = 
+  {
+    userName: 'world',
+    displayName: 'world'
+  }
 
-  styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-    }
-  });
+  checkEmpty = name => name.length > 0 ? name : 'world';
 
-  
- 
+  catchName = name => this.setState({userName: this.checkEmpty(name)});
+
+  sendName = ()=> this.setState({displayName: this.state.userName});
+
   render() {
     return (
-      <View style={this.styles.container}>
-        <Routes/>
+      <View style={styles.container}>
+        <Text>Hello {this.state.displayName}!</Text>
+        <TextInput placeholder='name' onChangeText={this.catchName}/>
+        <Button title='send name' onPress={this.sendName}/>
         <StatusBar style="auto"/>
       </View>    
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
